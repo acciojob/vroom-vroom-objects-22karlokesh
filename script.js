e9c7d9a6216432889f346920936b83df3e83bfed
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-06-02 20:48:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-// Complete the js code
-function Car(make, model) {}
+class Car {
+  constructor(make, model) {
+    this.make = make;
+	this.model = model;
+  }
+  getMakeModel(){
+	  return `${make} ${model}`;
+  }
+}
+class SportsCar extends Car {
+  constructor(make, model, topSpeed) {
+    super(make,model);
+	this.topSpeed = topSpeed;
+  }
+  getTopSpeed(){
+	  return topSpeed;
+  }
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Complete the js code
+// function Car(make, model) {
+	
+// }
+
+// function SportsCar(make, model, topSpeed) {
+	
+// }
 
 // Do not change the code below
 window.Car = Car;
